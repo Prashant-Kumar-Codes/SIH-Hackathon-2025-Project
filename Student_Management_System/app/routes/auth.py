22eff276179702ca5dddd91193bc525b512fc9a6
Prashant-Kumar-Codes <prashantkumar102369@gmail.com>
2025-11-02 18:46:28 +0530

    some minor changes

--- Student_Management_System/app/routes/auth.py
+++ Student_Management_System/app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from flask_mail import Message
-from flask_mail import Message
+from app import mail
 import random
 import mysql.connector
 from datetime import datetime, timedelta
